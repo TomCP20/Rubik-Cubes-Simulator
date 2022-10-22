76cfdee54b8a5c179f66476174b705c2f3725cb1
dei20 <107045601+dei20@users.noreply.github.com>
2022-10-22 19:44:56 +0100

    Cube Updater
    Added code for updating the cube graphics.

--- 3rd year project/Assets/Script/CubeController.cs
+++ 3rd year project/Assets/Script/CubeController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Threading;
 using System;
 using UnityEngine;
+using System.Diagnostics;
 
 public class CubeController : MonoBehaviour
 {
-    static double Magnitude(double[] position)
+    static double Magnitude(Vector3 position)
     {
         return Math.Abs(position[0]) + Math.Abs(position[1]) + Math.Abs(position[2]);
     }
@@ -31,16 +32,16 @@ public class CubeController : MonoBehaviour
 
     class Face
     {
-        private Colour colour;
-        private double[] direction; //x, y, z from the pice
+        public Colour colour;
+        public Vector3 direction;
 
-        public Face(Colour c, double[] d)
+        public Face(Colour c, Vector3 d)
         {
             colour = c;
             direction = d;
         }
 
-        public Face(double[] d)
+        public Face(Vector3 d)
         {
             direction = d;
             if (direction[0] > 0) { colour = Colour.Green; }
@@ -51,38 +52,37 @@ public class CubeController : MonoBehaviour
             else if (direction[2] < 0) { colour = Colour.Orange; }
             else { colour = Colour.Black; }
         }
+
     }
 
     class Pice
     {
-        private double[] position; //x, y, z
-        private Face[] faces;
+        public Vector3 position;
+        public Face[] faces;
 
-        public Pice(double[] p, Face[] f)
+        public Pice(Vector3 p, Face[] f)
         {
             position = p;
             faces = f;
         }
 
-        public Pice(double[] p)
+        public Pice(Vector3 pos)
         {
-            position = p;
-            double magnitude = Magnitude(position);
-            faces = new Face[(int)magnitude];
-            int count = 0;
+            position = pos;
+            List<Face> FaceList = new List<Face>();
             for (int i = 0; i < 3; i++)
             {
                 if (position[i] != 0)
                 {
-                    faces[count] = new Face(PositionToDirection(position, (Axis)i));
-                    count++;
+                    FaceList.Add(new Face(PositionToDirection(position, (Axis)i)));
                 }
             }
+            faces = FaceList.ToArray();
         }
 
-        private double[] PositionToDirection(double[] p, Axis axis)
+        private Vector3 PositionToDirection(Vector3 p, Axis axis)
         {
-            double[] direction = {0, 0, 0};
+            Vector3 direction = Vector3.zero;
             direction[(int)axis] = p[(int)axis] / 2;
             return direction;
         }
@@ -90,7 +90,7 @@ public class CubeController : MonoBehaviour
 
     class Cube
     {
-        private Pice[] pices;
+        public Pice[] pices;
 
         public Cube(Pice[] p)
         {
@@ -99,38 +99,96 @@ public class CubeController : MonoBehaviour
 
         public Cube()
         {
-            pices = new Pice[20];
-            int count = 0;
+            List<Pice> Picelist = new List<Pice>();
             double magnitude;
-            double[] position;
+            Vector3 position;
             for (int i = -1; i <= 1; i++)
             {
                 for (int j = -1; j <= 1; j++)
                 {
                     for (int k = -1; k <= 1; k++)
                     {
-                        position = new double[] { i, j, k};
+                        position = new Vector3(i, j, k);                        
                         magnitude = Magnitude(position);
                         if (magnitude == 3 || magnitude == 2)
                         {
-                            pices[count] = new Pice(position);
-                            count++;
+                            Picelist.Add(new Pice(position));
                         }
                     }
                 }
             }
+            pices = Picelist.ToArray();
         }
     }
+
+    Cube c;
+    bool cubeAltered;
+    public Material Black;
+    public Material White;
+    public Material Green;
+    public Material Blue;
+    public Material Red;
+    public Material Yellow;
+    public Material Orange;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cube c = new Cube();
+        
+        c = new Cube();
+
+        cubeAltered = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (cubeAltered) { updateCube(c); }
+        cubeAltered = false;
+        
+    }
+
+    void updateCube(Cube c)
+    {
+        UnityEngine.Debug.Log("updating cube");
+        foreach (Pice p in c.pices)
+        {
+            foreach (Face f in p.faces)
+            {
+                Vector3 coords = p.position + f.direction;
+                UnityEngine.Debug.Log(coords);
+                Collider[] hitColliders = Physics.OverlapSphere(coords, 0.1f);
+                foreach (var hitCollider in hitColliders)
+                {
+                    UnityEngine.Debug.Log(hitCollider);
+                    hitCollider.GetComponent<MeshRenderer>().material = GetMaterial(f.colour);
+                }
+            }
+        }
+    }
+
+    Material GetMaterial(Colour c)
+    {
+        switch(c)
+        {
+            case Colour.Blue:
+                return Blue;
+            case Colour.Green:
+                return Green;
+            case Colour.Red:
+                return Red;
+            case Colour.Yellow:
+                return Yellow;
+            case Colour.Orange:
+                return Orange;
+            case Colour.White:
+                return White;
+            case Colour.Black:
+                return Black;
+            default:
+                return null;
+        }
     }
 }

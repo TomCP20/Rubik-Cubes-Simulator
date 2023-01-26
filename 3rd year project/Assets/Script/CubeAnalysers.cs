2d172dc94a5000ffdd5efeb56ac89f4fe88243a3
dei20 <107045601+dei20@users.noreply.github.com>
2023-01-26 17:02:43 +0000

    script folder + analyser
    restructured script folder + started on  solver analyser

---
+++ 3rd year project/Assets/Script/CubeAnalysers.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Cubes;
+using LayerByLayers;
+using Moves;
+
+namespace CubeAnalysers
+{
+    public class CubeAnalyser
+    {
+        Cube c;
+        LayerByLayer s;
+
+        Queue<Move> moves;
+
+        public CubeAnalyser()
+        {
+            Cube c = new Cube();
+            c.randomMoveSequence();
+            LayerByLayer s = new LayerByLayer(c);
+            moves = s.getSolution();
+        }
+
+        public int getHTM()
+        {
+            return moves.Count;
+        }
+
+        public int getQTM()
+        {
+            int sum = 0;
+            foreach (Move m in moves)
+            {
+                sum += Math.Abs(m.angle);
+            }
+            return sum;
+        }
+
+        public int getSTM()
+        {
+            Queue<Move> newQueue = new Queue<Move>(moves);
+            int sum = 0;
+            Move current = newQueue.Dequeue();
+            while (newQueue.Count > 0)
+            {
+                Move next = newQueue.Dequeue();
+                if (current.axis == next.axis && current.angle == next.angle)
+                {
+                    sum+=1;
+                    current = newQueue.Dequeue();
+                }
+                else
+                {
+                    sum+=1;
+                    current = next;
+                }
+            }
+            sum+=1;
+            return sum;
+        }
+
+        public int getQSTM()
+        {
+            Queue<Move> newQueue = new Queue<Move>(moves);
+            int sum = 0;
+            Move current = newQueue.Dequeue();
+            while (newQueue.Count > 0)
+            {
+                Move next = newQueue.Dequeue();
+                if (current.axis == next.axis && current.angle == next.angle)
+                {
+                    sum+=current.angle;
+                    current = newQueue.Dequeue();
+                }
+                else
+                {
+                    sum+=current.angle;
+                    current = next;
+                }
+            }
+            sum+=current.angle;
+            return sum;
+        }
+
+        public int getATM()
+        {
+            Queue<Move> newQueue = new Queue<Move>(moves);
+            int sum = 0;
+            Move current = newQueue.Dequeue();
+            while (newQueue.Count > 0)
+            {
+                Move next = newQueue.Dequeue();
+                if (current.axis == next.axis)
+                {
+                    current = next;
+                }
+                else
+                {
+                    sum+=1;
+                    current = next;
+                }
+            }
+            sum+=1;
+            return sum;
+        }
+
+        public double get15HTM()
+        {
+            double sum = 0;
+            foreach (Move m in moves)
+            {
+                if (m.angle == 2)
+                {
+                    sum += 1.5;
+                }
+                else
+                {
+                    sum += 1;
+                }
+            }
+            return sum;
+        }
+    }
+}

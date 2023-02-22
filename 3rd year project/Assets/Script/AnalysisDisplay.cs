d215572f5ca9e77c895cd1052901d8a51ac53ba7
dei20 <107045601+dei20@users.noreply.github.com>
2023-02-22 15:11:38 +0000

    Improved analysis

--- 3rd year project/Assets/Script/AnalysisDisplay.cs
+++ 3rd year project/Assets/Script/AnalysisDisplay.cs
@@ -9,19 +9,39 @@ using CubeAnalysers;
 public class AnalysisDisplay : MonoBehaviour
 {
     private int cubeNo;
-    public TextMeshProUGUI t;
+    [SerializeField] private TextMeshProUGUI text;
+
+    //public TMPro.TMP_Dropdown dropdown;
+    [SerializeField] private TMP_Dropdown d;
+
+    int type;
+
+    private void Start()
+    {
+        type = 0;
+        text = GameObject.Find("output (TMP)").GetComponent<TextMeshProUGUI>();
+        d = GameObject.Find("Dropdown").GetComponent<TMP_Dropdown>();
+    }
+
+    public void updateType(TMP_Dropdown dropdown)
+    {
+        type = dropdown.value;
+    }
     public void Analysis()
     {
         cubeNo = 100;
-        float[] avgs = calcAvg();
-        t.text = getOutput(avgs);
+        type = d.value;
+        float[][] results = getResults(type);
+        float[] avgs = calcAvg(results);
+        float[] standardDeviation = calcSD(avgs, results);
+        text.text = getOutput(avgs, standardDeviation);
     }
 
-    private float[] calcAvg()
+    private float[] calcAvg(float[][] results)
     {
         float[] sums = new float[6];
         float[] avgs = new float[6];
-        float[][] results = getResults();
+        
         for (int i = 0; i < cubeNo; i++)
         {
             for (int j = 0; j < 6; j++)
@@ -36,25 +56,43 @@ public class AnalysisDisplay : MonoBehaviour
         return avgs;
     }
 
-    private string getOutput(float[] avgs)
+    private float[] calcSD(float[] avgs, float[][] results)
+    {
+        float[] variance = new float[6];
+        float[] standardDeviation = new float[6];
+        for (int i = 0; i < cubeNo; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                variance[j] += Mathf.Pow((results[i][j] - avgs[j]), 2)/cubeNo;
+            }
+        }
+        for (int j = 0; j < 6; j++)
+        {
+            standardDeviation[j] = Mathf.Sqrt(variance[j]);
+        }
+        return standardDeviation;
+    }
+
+    private string getOutput(float[] avgs, float[] standardDeviation)
     {
         string output = "";
-        output += "Avearge HTM of: " + avgs[0] + "\n";
-        output += "Avearge QTM of: " + avgs[1] + "\n";
-        output += "Avearge STM of: " + avgs[2] + "\n";
-        output += "Avearge QSTM of: " + avgs[3] + "\n";
-        output += "Avearge ATM of: " + avgs[4] + "\n";
-        output += "Avearge 1.5HTM of: " + avgs[5] + "\n";
+        output += "Avearge HTM of: " + avgs[0] + " and σ of " + standardDeviation[0] + "\n";
+        output += "Avearge QTM of: " + avgs[1] + " and σ of " + standardDeviation[1] + "\n";
+        output += "Avearge STM of: " + avgs[2] + " and σ of " + standardDeviation[2] + "\n";
+        output += "Avearge QSTM of: " + avgs[3] + " and σ of " + standardDeviation[3] + "\n";
+        output += "Avearge ATM of: " + avgs[4] + " and σ of " + standardDeviation[4] + "\n";
+        output += "Avearge 1.5HTM of: " + avgs[5] + " and σ of " + standardDeviation[5] + "\n";
         return output;
     }
 
-    private float[][] getResults()
+    private float[][] getResults(int type)
     {
         float[][] results = new float[cubeNo][];
 
         for (int i = 0; i < cubeNo; i++)
         {
-            CubeAnalyser c = new CubeAnalyser(1);
+            CubeAnalyser c = new CubeAnalyser(type);
             results[i] = c.getCount();
         }
         return results;

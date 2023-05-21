afd47904a0ed3602a36baf6c3bd6ebc458e35d5e
dei20 <107045601+dei20@users.noreply.github.com>
2023-05-21 17:58:32 +0100

    variable name change

--- 3rd year project/Assets/Script/Solver/CubeSolver.cs
+++ 3rd year project/Assets/Script/Solver/CubeSolver.cs
@@ -109,14 +109,14 @@ public abstract class CubeSolver
         }
     }
 
-    protected void rotateToCorrectPosition(Piece whiteCorner)
+    protected void rotateToCorrectPosition(Piece piece)
     {
-        Vector3 targetPos = whiteCorner.SolvedPosition();
-        rotateToCorrectPosition(whiteCorner, targetPos);
+        Vector3 targetPos = piece.SolvedPosition();
+        rotateToCorrectPosition(piece, targetPos);
     }
-    protected void rotateToCorrectPosition(Piece whiteCorner, Vector3 targetPos)
+    protected void rotateToCorrectPosition(Piece piece, Vector3 targetPos)
     {
-        Vector3 currentPos = whiteCorner.position;
+        Vector3 currentPos = piece.position;
         Vector3 rot1 = Vector3Int.RoundToInt(Quaternion.Euler(0, 90, 0) * currentPos);
         Vector3 rot2 = Vector3Int.RoundToInt(Quaternion.Euler(0, 180, 0) * currentPos);
         Vector3 rot3 = Vector3Int.RoundToInt(Quaternion.Euler(0, 270, 0) * currentPos);

85691cbdeca5a50f598b0dbbc265af8f0ce6bdaa
dei20 <107045601+TomCP20@users.noreply.github.com>
2023-12-06 14:05:37 +0000

    Moved 2 files and added comments to screenshot

--- Rubik Cubes Simulator/Assets/Scripts/Screenshot.cs
+++ Rubik Cubes Simulator/Assets/Scripts/Screenshot.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Screenshot : MonoBehaviour
+/*
+A simple program that checks when the user presses the tab key and saves a screenshot.
+The Screenshot is saved with a name in the format of Screenshot-yyyy-MM-dd-HH-mm-ss.png.
+*/
+namespace Screenshot
 {
-    void Update()
+    public class Screenshot : MonoBehaviour
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
+        void Update()
         {
-            string date = System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
-            ScreenCapture.CaptureScreenshot(string.Format("Screenshot-{0}.png", date));
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                string date = System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+                ScreenCapture.CaptureScreenshot(string.Format("Screenshot-{0}.png", date));
+            }
         }
     }
 }
